747558cf1e7bd3f0e07cab55f88df916e8361dac
Gre Sastre <gretsastre@gmail.com>
2026-05-02 13:12:58 -0400

    feat: add login tests for saucedemo

---
+++ tests/example.spec.ts
@@ -0,0 +1,42 @@
+import { test, expect } from '@playwright/test';
+ 
+test ('Login Exitoso', async({page}) => {
+
+  await page.goto('https://www.saucedemo.com/');
+  await page.getByPlaceholder('Username').fill('standard_user');
+
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await expect(page).toHaveURL(/inventory/);
+
+
+})
+ //Test fallido co usuario incorrecto
+test ('Login Fallido', async({page}) => {
+
+  await page.goto('https://www.saucedemo.com/');
+  await page.getByPlaceholder('Username').fill('usuario_falso');
+
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await expect(page.getByText('Username and password do not match')).toBeVisible();
+
+
+})
+ 
+//test fallidos con campos vacios 
+test ('Login Fallido-campos vacios', async({page}) => {
+
+  await page.goto('https://www.saucedemo.com/');
+
+
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await expect(page.getByText('Username is required')).toBeVisible();
+
+
+})
