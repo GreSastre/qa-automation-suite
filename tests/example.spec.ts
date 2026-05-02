ff9c04e08f865f2b87da82b4f282e3257aa0d1eb
Gre Sastre <gretsastre@gmail.com>
2026-05-02 13:25:50 -0400

    refactor: add beforeEach to avoid duplicate goto

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,8 +1,12 @@
 import { test, expect } from '@playwright/test';
  
+test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+});
+
 test ('Login Exitoso', async({page}) => {
 
-  await page.goto('https://www.saucedemo.com/');
+  
   await page.getByPlaceholder('Username').fill('standard_user');
 
   await page.getByPlaceholder('Password').fill('secret_sauce');
@@ -16,7 +20,7 @@ test ('Login Exitoso', async({page}) => {
  //Test fallido co usuario incorrecto
 test ('Login Fallido', async({page}) => {
 
-  await page.goto('https://www.saucedemo.com/');
+
   await page.getByPlaceholder('Username').fill('usuario_falso');
 
   await page.getByPlaceholder('Password').fill('secret_sauce');
@@ -31,12 +35,27 @@ test ('Login Fallido', async({page}) => {
 //test fallidos con campos vacios 
 test ('Login Fallido-campos vacios', async({page}) => {
 
-  await page.goto('https://www.saucedemo.com/');
 
 
   await page.getByRole('button', {name: 'Login'}).click();
 
   await expect(page.getByText('Username is required')).toBeVisible();
+
+
+})
+
+//test para usuario bloqueado 
+test ('Login Fallido-Usuario bloqueado', async({page}) => {
+
+ 
+
+  await page.getByPlaceholder('Username').fill('locked_out_user');
+
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await expect(page.getByText('Sorry, this user has been locked out')).toBeVisible();
 
 
 })
