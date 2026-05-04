bdcffbeefb57d78d5de039c5b81280675483a68f
Gre Sastre <gretsastre@gmail.com>
2026-05-04 17:08:38 -0400

    feat: add purchase flow tests and wait strategies

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
- 
-test.beforeEach(async ({ page }) => {
+test.describe('Login-Sauce demo', ()=>{
+
+  test.beforeEach(async ({ page }) => {
   await page.goto('https://www.saucedemo.com/');
 });
 
@@ -27,9 +28,7 @@ test ('Login Fallido', async({page}) => {
 
   await page.getByRole('button', {name: 'Login'}).click();
 
-  await expect(page.getByText('Username and password do not match')).toBeVisible();
-
-
+  await expect(page.locator('[data-test="error"]')).toContainText('Username and password do not match');
 })
  
 //test fallidos con campos vacios 
@@ -39,7 +38,7 @@ test ('Login Fallido-campos vacios', async({page}) => {
 
   await page.getByRole('button', {name: 'Login'}).click();
 
-  await expect(page.getByText('Username is required')).toBeVisible();
+  await expect(page.locator('[data-test="error"]')).toBeVisible(); 
 
 
 })
@@ -55,7 +54,61 @@ test ('Login Fallido-Usuario bloqueado', async({page}) => {
 
   await page.getByRole('button', {name: 'Login'}).click();
 
-  await expect(page.getByText('Sorry, this user has been locked out')).toBeVisible();
+  await expect(page.locator('[data-test="error"]')).toContainText('locked out');
+
+
+ })
+})
+
+
+test.describe('Flujo de compra en Saucedemo', ()=>{
+ 
+ 
+ //login 
+  test.beforeEach(async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+
+  await page.getByPlaceholder('Username').fill('standard_user');
+
+  await page.getByPlaceholder('Password').fill('secret_sauce');
+
+  await page.getByRole('button', {name: 'Login'}).click();
+
+  await expect(page).toHaveURL(/inventory/);
+});
+
+
+// agregar producto al carrito 
+test('Pedido Exitso', async({page})=>{
+//Agregar producto al carrito
+await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+
+//verificar que el carrito no este vacio
+await expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+
+//ir al carrito de compra
+await page.locator('[data-test="shopping-cart-link"]').click();
+
+ //ir a checkout
+ await page.locator('[data-test="checkout"]').click();
+ //rellenar campos de datos 
+await page.getByPlaceholder('First Name').fill('Gre');
+await page.getByPlaceholder('Last Name').fill('Sastre');
+await page.getByPlaceholder('Zip/Postal Code').fill('32807');
+await page.getByRole("button", {name:'Continue'}).click();
+await page.locator('[data-test="finish"]').click();
+await expect(page.locator('[data-test="complete-header"]')).toHaveText('Thank you for your order!');
+
+
+})
+
+test('redireccion correcta despues del login', async ({ page }) => {
+
+  await page.waitForURL(/inventory/);
+  await expect(page.getByText('Products')).toBeVisible();
+});
+
+
 
 
 })
